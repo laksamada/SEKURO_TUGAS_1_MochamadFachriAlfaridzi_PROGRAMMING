df239efc2991825e39568fda9ef6a01e369baf2f
laksamada <laksamada.dev@gmail.com>
2026-03-25 22:04:37 +0700

    Adjust OOP scope

---
+++ src/oop_soal_1_alternatif/mobil.cpp
@@ -0,0 +1,57 @@
+#include "mobil.hpp"
+
+mobil::mobil(int kapasitas_mesin, string name, string merk, int top_speed, string bahan_bakar) {
+    this->kapasitas_mesin = kapasitas_mesin;
+    this->name = name;
+    this->merk = merk;
+    this->top_speed = top_speed;
+    this->bahan_bakar = bahan_bakar;
+    cout << name << " keluar dari pabrik " << merk << " nih gaspol ah" << endl;
+}
+
+void mobil::jalan(int jarak) {
+    cout << "Mobil " << name << " sedang jalan sejauh " << jarak << " kilometer" << endl;
+}
+
+void mobil::pamer_mesin() {
+    if (kapasitas_mesin < 2000) {
+        cout << "Yah " << name << " kagak bisa ngeraung cc mesinnya kurang" << endl;
+    } else {
+        cout << "WRAAAUR " << name << " meraung dengan kerennya" << endl;
+    }
+}
+
+void mobil::cek_spesifikasi() {
+    cout << "=== Spesifikasi Standar " << name << " ===" << endl;
+    cout << "Merk: " << merk << " | Mesin: " << kapasitas_mesin << " CC" << endl;
+    cout << "Top Speed: " << top_speed << " km/jam | BBM: " << bahan_bakar << endl;
+}
+
+mobil::~mobil() {
+    cout << "BOOM " << name << " Dihancurin" << endl;
+}
+
+mobil_sport::mobil_sport(int kapasitas_mesin, string name, string merk, int top_speed, string bahan_bakar)
+    : mobil(kapasitas_mesin, name, merk, top_speed, bahan_bakar) {
+    turbo_aktif = false;
+}
+
+void mobil_sport::jalan(int jarak) {
+    cout << "Wuzzz! Sportscar " << name << " melesat sejauh " << jarak << " km!" << endl;
+}
+
+void mobil_sport::cek_spesifikasi() {
+    cout << "=== Spesifikasi High-Performance " << name << " ===" << endl;
+    cout << "Merk: " << merk << " | Mesin: " << kapasitas_mesin << " CC" << endl;
+    cout << "Kecepatan Maksimum: " << top_speed << " km/jam | BBM: " << bahan_bakar << endl;
+    if (turbo_aktif) {
+        cout << "Status Turbo: Aktif" << endl;
+    } else {
+        cout << "Status Turbo: Non-aktif" << endl;
+    }
+}
+
+void mobil_sport::aktifkan_turbo() {
+    turbo_aktif = true;
+    cout << "Turbo diaktifkan! Power " << name << " meningkat drastis!" << endl;
+}
